d54962797e4bf833efc32ac57f97dd2bccfb331d
Yunus Kucuk <yunus.kucuk.dev@gmail.com>
2026-01-05 17:49:01 +0300

    Contact sayfası dinamik hale getirildi

--- Udemy/Udemy.WebUI/Controllers/ContactController.cs
+++ Udemy/Udemy.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using Udemy.DtoLayer.CatalogDtos.ContactDtos;
 
 namespace Udemy.WebUI.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Index(CreateContactDto dto)
+        {
+            dto.IsRead = false;
+            dto.SendDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+
             return View();
         }
     }
